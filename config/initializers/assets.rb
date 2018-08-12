9d0f0da22f32d8e434df5f2843f74477f88fbc65
yelbmew <yelbmewo@gmail.com>
2018-08-13 04:01:58 +1000

    add bootstrap, jquery

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,31 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+
+# //= require main
+# //= require jquery.magnific-popup.min
+# //= require owl.carousel.min
+# //= require superfish.min
+# //= require jquery.nice-select.min
+# //= require bootstrap.min
+# //= require jquery-2.2.4.min
+# //= require jquery
+# //= require jquery2
+# //= require jquery_ujs
+
+# for bootstrap.css
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+
+# for home menu selection css
+Rails.application.config.assets.precompile += %w( nice-select.css )
+
+# owl carousel
+Rails.application.config.assets.precompile += %w( owl.carousel.css )
+
+# jquerysctipttop
+Rails.application.config.assets.precompile += %w( jquerysctipttop.css )
+
+# linearicons
+Rails.application.config.assets.precompile += %w( linearicons.css )
