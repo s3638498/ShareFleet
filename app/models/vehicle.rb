c0e2725a4ea7263124007ecb997eaef263f25b2e
Ryan Tran <s3201690@student.rmit.edu.au>
2018-08-27 14:50:16 +0000

    Added Unit testing for Vehicle

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -3,4 +3,18 @@ class Vehicle < ApplicationRecord
     has_many_attached :images
     
     scope :with_eager_loaded_images, -> { eager_load(images_attachments: :blob) }
+    
+    VALID_REGO_REGEX = /\A[A-Z0-9]*\z/
+    validates :registration, presence: true, uniqueness: true,
+    length: { minimum:6, maximum: 6 },
+    format: { with: VALID_REGO_REGEX ,
+    message: "Can only contain Capital letters and digits." }
+    
+    validates :body, presence: true
+    validates :make, presence: true
+    validates :model, presence: true
+    validates :odometer, presence: true
+    validates :year, presence: true
+    validates :colour, presence: true
+    validates :status, presence: true
 end
--- test/models/vehicle_test.rb
+++ test/models/vehicle_test.rb
@@ -11,12 +11,70 @@ class VehicleTest < ActiveSupport::TestCase
       make: "Honda",
       model: "Civic",
       odometer: "2002",
-      year: "2017",
+      year: Date.new(2017, 1, 1),
       colour: "Blue",
       status: "Available",
       )
   end
-  test "should be valid" do
+  
+  test "Vehicle should be valid" do
     assert @vehicle_valid.valid?
+  end
+  
+  test "registration should be present" do
+    @vehicle_valid.registration = "     "
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "registration too short" do
+    @vehicle_valid.registration = "A" * 5
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "registration too long" do
+    @vehicle_valid.registration = "A" * 7
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "registration reject if not 6 letters or isn't digits and capital letters only" do
+    invalid_regos = %w[aaa123 Aaa123 "aa-aaa" 1234567 123s45]
+    invalid_regos.each do |invalid_rego|
+      @vehicle_valid.registration = invalid_rego
+      assert_not @vehicle_valid.valid?, "#{invalid_rego.inspect} should be invalid"
+    end
+  end
+  
+  test "body should be present" do
+    @vehicle_valid.body = "     "
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "make should be present" do
+    @vehicle_valid.make = "     "
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "model should be present" do
+    @vehicle_valid.model = "     "
+    assert_not @vehicle_valid.valid?
+  end
+  test "odometer should be present" do
+    @vehicle_valid.odometer = "     "
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "year should be present" do
+    @vehicle_valid.year = nil
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "colour should be present" do
+    @vehicle_valid.colour = "     "
+    assert_not @vehicle_valid.valid?
+  end
+  
+  test "status should be present" do
+    @vehicle_valid.status = "     "
+    assert_not @vehicle_valid.valid?
   end
 end
