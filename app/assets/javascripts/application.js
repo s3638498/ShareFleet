a766fe529db841d191347ece1d539ac05b54071d
wembleyS3482893 <s3482893@student.rmit.edu.au>
2018-09-06 06:47:34 +1000

    regular backup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -78,10 +78,10 @@ $(document).ready(function(){
   });
 
 
-    $( function() {
-        $( "#datepicker" ).datepicker();
-        $( "#datepicker2" ).datepicker();
-     });
+    //$( function() {
+    //    $( "#datepicker" ).datepicker();
+    //    $( "#datepicker2" ).datepicker();
+    // });
   
 
   // Mobile Navigation
@@ -228,56 +228,146 @@ $(document).ready(function(){
         }
     });
 
-
-       
-
-
-    //  Start Google map 
-
-            // When the window has finished loading create our google map below
-
-            if(document.getElementById("map")){
-            
-            google.maps.event.addDomListener(window, 'load', init);
-        
-            function init() {
-                // Basic options for a simple Google Map
-                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
-                    zoom: 11,
-
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(40.6700, -73.9400), // New York
-
-                    // How you would like to style the map. 
-                    // This is where you would paste any style found on Snazzy Maps.
-                    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-                };
-
-                // Get the HTML DOM element that will contain your map 
-                // We are using a div with id="map" seen below in the <body>
-                var mapElement = document.getElementById('map');
-
-                // Create the Google Map using our element and options defined above
-                var map = new google.maps.Map(mapElement, mapOptions);
-
-                // Let's also add a marker while we're at it
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(40.6700, -73.9400),
-                    map: map,
-                    title: 'Snazzy!'
-                });
-            }
-    }
-
-
-        $(document).ready(function() {
-            $('#mc_embed_signup').find('form').ajaxChimp();
-        });      
-
  });
 
 
+function initMap() {
+  // latitude and logitude for different location
+  // var name in location x streetName
 
+  //3000 City
+  var cityxmc = {lat: -37.811316, lng: 144.962345};
+  var cityxeli = {lat: -37.812621, lng: 144.961197};
+  var cityxflin = {lat: -37.817611, lng: 144.964273};
+  var cityxltbourke = {lat: -37.816001, lng: 144.953093};
+  var cityxspring = {lat: -37.813479, lng: 144.973582};
+
+  //3002 East Melbourne
+  var eastxalbert = {lat: -37.810182, lng: 144.981044};
+
+  //3008 Docklands
+  var dockxbourke = {lat: -37.819007, lng: 144.946385};
+  var dockxlatrobe = {lat: -37.814998, lng: 144.945537};
+  
+  //3053 Carlton
+  var carlxlygon = {lat: -37.798246, lng: 144.967267};
+  var carlxgrattan = {lat: -37.800836, lng: 144.967508};
+
+  //map, Melbourne Central 
+  var map = new google.maps.Map(
+      document.getElementById('googleMaps'), {
+      zoom: 17, 
+      center: cityxeli
+      });
+
+  // The red marker, positioned for City 3000
+  var marker = new google.maps.Marker({position: cityxmc, map: map});
+  var marker = new google.maps.Marker({position: cityxeli, map: map});
+  var marker = new google.maps.Marker({position: cityxflin, map: map});
+  var marker = new google.maps.Marker({position: cityxltbourke, map: map});
+  var marker = new google.maps.Marker({position: cityxspring, map: map});
+
+  // The red marker, positioned for East Melbourne 3002
+  var marker = new google.maps.Marker({position: eastxalbert, map: map});
+
+  // The red marker, positioned for Docklands 3008
+  var marker = new google.maps.Marker({position: dockxbourke, map: map});
+  var marker = new google.maps.Marker({position: dockxlatrobe, map: map});
+
+  // The red marker, positioned for Carlton 3053
+  var marker = new google.maps.Marker({position: carlxlygon, map: map});
+  var marker = new google.maps.Marker({position: carlxgrattan, map: map});
+
+  // Set autocomplete project
+  var defaultBounds = new google.maps.LatLngBounds(
+      //new google.maps.LatLng(-33.8902, 151.1759),
+      //new google.maps.LatLng(-33.8474, 151.2631));
+      
+      // set not boundaries
+     new google.maps.LatLng(-90, -180),
+     new google.maps.LatLng(90, 180));
+  
+  var input = document.getElementById('pac-input');
+  var options = {
+      bounds: defaultBounds,
+      // set location only in AU
+      //types: ['(cities)'],
+      //componentRestrictions: {country: 'au'}
+  };
+
+  // Create the autocomplete object
+  var autocomplete = new google.maps.places.Autocomplete(input, options);
+
+  autocomplete.bindTo('bounds', map);
+
+  autocomplete.setFields(
+    ['address_components', 'geometry', 'icon', 'name']);
+
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
+  infowindow.setContent(infowindowContent);
+  var marker = new google.maps.Marker({
+    map:map,
+    anchorPoint: new google.maps.Point(0, -29)
+  });
+
+       autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            // User entered the name of a Place that was not suggested and
+            // pressed the Enter key, or the Place Details request failed.
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindowContent.children['place-icon'].src = place.icon;
+          infowindowContent.children['place-name'].textContent = place.name;
+          infowindowContent.children['place-address'].textContent = address;
+          infowindow.open(map, marker);
+        });
+
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+        function setupClickListener(id, types) {
+          var radioButton = document.getElementById(id);
+          radioButton.addEventListener('click', function() {
+            autocomplete.setTypes(types);
+          });
+        }
+
+        setupClickListener('changetype-all', []);
+        setupClickListener('changetype-address', ['address']);
+        setupClickListener('changetype-establishment', ['establishment']);
+        setupClickListener('changetype-geocode', ['geocode']);
+
+        document.getElementById('use-strict-bounds')
+            .addEventListener('click', function() {
+              console.log('Checkbox clicked! New state=' + this.checked);
+              autocomplete.setOptions({strictBounds: this.checked});
+            });
+
+}
+
+ 
 
