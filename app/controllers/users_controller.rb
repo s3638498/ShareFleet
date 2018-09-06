dec35991ecad9cdf836e8a8969150c585fc657f6
s3638498 <s3638498@student.rmit.edu.au>
2018-09-06 03:14:42 +0000

    fix bug on controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy,:deactive,:reactivate]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user,   only: [:show, :edit, :destroy]
-  before_action :admin_user,     only: [:index, :destroy, :deactivate, :reactivate,:edit]
+  before_action :admin_user,     only: [:index, :destroy, :deactivate, :reactivate]
 
   # GET /users
   def signup
