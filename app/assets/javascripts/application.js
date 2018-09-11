32b9d0f2a072b3bf12d940a4b721a813a2e6e796
wembleyS3482893 <s3482893@student.rmit.edu.au>
2018-09-11 15:05:25 +1000

    implement car locator on Google Maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,9 +23,6 @@
 //= require superfish.min
 //= require jquery-2.2.4.min
 
-
-
-
 $(document).ready(function(){
 	"use strict";
 
@@ -39,35 +36,31 @@ $(document).ready(function(){
 	$(".fullscreen").css("height", window_height)
 	$(".fitscreen").css("height", fitscreen);
 
-     if(document.getElementById("default-select")){
-          $('select').niceSelect();
-    };
+ if(document.getElementById("default-select")){
+  $('select').niceSelect();
+};
 
-    $('.img-pop-up').magnificPopup({
-        type: 'image',
-        gallery:{
-        enabled:true
-        }
-    });
+$('.img-pop-up').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
 
-    $('.single-gallery').magnificPopup({
-        type: 'image',
-        gallery:{
-        enabled:true
-        }
-    });
-
+$('.single-gallery').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
 
     //  Counter Js 
-     if(document.getElementById("facts-area")){
-          $('.counter').counterUp({
-          delay: 10,
-          time: 1000
-       });
-     };
-
-
-
+    if(document.getElementById("facts-area")){
+      $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+      });
+    };
 
   // Initiate superfish on nav menu
   $('.nav-menu').superfish({
@@ -82,47 +75,8 @@ $(document).ready(function(){
     //    $( "#datepicker" ).datepicker();
     //    $( "#datepicker2" ).datepicker();
     // });
-  
 
-  // Mobile Navigation
-  if ($('#nav-menu-container').length) {
-    var $mobile_nav = $('#nav-menu-container').clone().prop({
-      id: 'mobile-nav'
-    });
-    $mobile_nav.find('> ul').attr({
-      'class': '',
-      'id': ''
-    });
-    $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="lnr lnr-menu"></i></button>');
-    $('body').append('<div id="mobile-body-overly"></div>');
-    $('#mobile-nav').find('.menu-has-children').prepend('<i class="lnr lnr-chevron-down"></i>');
 
-    $(document).on('click', '.menu-has-children i', function(e) {
-      $(this).next().toggleClass('menu-item-active');
-      $(this).nextAll('ul').eq(0).slideToggle();
-      $(this).toggleClass("lnr-chevron-up lnr-chevron-down");
-    });
-
-    $(document).on('click', '#mobile-nav-toggle', function(e) {
-      $('body').toggleClass('mobile-nav-active');
-      $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-      $('#mobile-body-overly').toggle();
-    });
-
-    $(document).click(function(e) {
-      var container = $("#mobile-nav, #mobile-nav-toggle");
-      if (!container.is(e.target) && container.has(e.target).length === 0) {
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
-          $('#mobile-body-overly').fadeOut();
-        }
-      }
-    });
-  } else if ($("#mobile-nav, #mobile-nav-toggle").length) {
-    $("#mobile-nav, #mobile-nav-toggle").hide();
-  }
 
   // Smooth scroll for the menu and links with .scrollto classes
   $('.nav-menu a, #mobile-nav a, .scrollto').on('click', function() {
@@ -159,34 +113,34 @@ $(document).ready(function(){
   });
 
 
-    $(document).ready(function() {
+$(document).ready(function() {
 
-    $('html, body').hide();
+  $('html, body').hide();
 
-        if (window.location.hash) {
+  if (window.location.hash) {
 
-        setTimeout(function() {
+    setTimeout(function() {
 
-        $('html, body').scrollTop(0).show();
+      $('html, body').scrollTop(0).show();
 
-        $('html, body').animate({
+      $('html, body').animate({
 
         scrollTop: $(window.location.hash).offset().top-62
 
-        }, 1000)
+      }, 1000)
 
-        }, 0);
+    }, 0);
 
-        }
+  }
 
-        else {
+  else {
 
-        $('html, body').show();
+    $('html, body').show();
 
-        }
+  }
 
-    });
-  
+});
+
 
   // Header scroll class
   $(window).scroll(function() {
@@ -198,42 +152,45 @@ $(document).ready(function(){
   })
 
 
-    $('.active-model-carusel').owlCarousel({
-        items:1,
-        loop:true,
-        margin:30,
-        dots: true
-    });
+  $('.active-model-carusel').owlCarousel({
+    items:1,
+    loop:true,
+    margin:30,
+    dots: true
+  });
 
-    $('.active-exibition-carusel').owlCarousel({
-        items:3,
-        margin:30,
-        autoplay:true,
-        loop:true,
-        dots: true,       
-            responsive: {
-            0: {
-                items: 1
-            },
-            480: {
-                items: 1,
-            },
-            768: {
-                items: 2,
-            },
-            900: {
-                items: 3,
-            }
+  $('.active-exibition-carusel').owlCarousel({
+    items:3,
+    margin:30,
+    autoplay:true,
+    loop:true,
+    dots: true,       
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      900: {
+        items: 3,
+      }
 
-        }
-    });
+    }
+  });
 
- });
+});
 
-
+//Google Maps
 function initMap() {
-  // latitude and logitude for different location
-  // var name in location x streetName
+  // latitude and longitude for different location
+  // var name format: location x streetName
+
+ // car marker hosted on imgur
+  var carMarker = "https://i.imgur.com/7Vts37R.png";
 
   //3000 City
   var cityxmc = {lat: -37.811316, lng: 144.962345};
@@ -255,119 +212,97 @@ function initMap() {
 
   //map, Melbourne Central 
   var map = new google.maps.Map(
-      document.getElementById('googleMaps'), {
+    document.getElementById('googleMaps'), {
       zoom: 17, 
       center: cityxeli
-      });
+    });
 
   // The red marker, positioned for City 3000
-  var marker = new google.maps.Marker({position: cityxmc, map: map});
-  var marker = new google.maps.Marker({position: cityxeli, map: map});
-  var marker = new google.maps.Marker({position: cityxflin, map: map});
-  var marker = new google.maps.Marker({position: cityxltbourke, map: map});
-  var marker = new google.maps.Marker({position: cityxspring, map: map});
+  var marker = new google.maps.Marker({position: cityxmc, map: map, icon: carMarker});
+  var marker = new google.maps.Marker({position: cityxeli, map: map, icon: carMarker});
+  var marker = new google.maps.Marker({position: cityxflin, map: map, icon: carMarker});
+  var marker = new google.maps.Marker({position: cityxltbourke, map: map, icon: carMarker});
+  var marker = new google.maps.Marker({position: cityxspring, map: map, icon: carMarker});
 
   // The red marker, positioned for East Melbourne 3002
-  var marker = new google.maps.Marker({position: eastxalbert, map: map});
+  var marker = new google.maps.Marker({position: eastxalbert, map: map, icon: carMarker});
 
   // The red marker, positioned for Docklands 3008
-  var marker = new google.maps.Marker({position: dockxbourke, map: map});
-  var marker = new google.maps.Marker({position: dockxlatrobe, map: map});
+  var marker = new google.maps.Marker({position: dockxbourke, map: map, icon: carMarker});
+  var marker = new google.maps.Marker({position: dockxlatrobe, map: map, icon: carMarker});
 
   // The red marker, positioned for Carlton 3053
-  var marker = new google.maps.Marker({position: carlxlygon, map: map});
-  var marker = new google.maps.Marker({position: carlxgrattan, map: map});
+  var marker = new google.maps.Marker({position: carlxlygon, map: map, icon: carMarker});
+  var marker = new google.maps.Marker({position: carlxgrattan, map: map, icon: carMarker});
 
+  // for assignment purpose set no boundaries -> -90, -180 90,180
   // Set autocomplete project
   var defaultBounds = new google.maps.LatLngBounds(
-      //new google.maps.LatLng(-33.8902, 151.1759),
-      //new google.maps.LatLng(-33.8474, 151.2631));
-      
-      // set not boundaries
-     new google.maps.LatLng(-90, -180),
-     new google.maps.LatLng(90, 180));
+   new google.maps.LatLng(-90, -180),
+   new google.maps.LatLng(90, 180));
   
   var input = document.getElementById('pac-input');
   var options = {
-      bounds: defaultBounds,
+    bounds: defaultBounds,
+      // for future reference if anyone want to set to any country
       // set location only in AU
       //types: ['(cities)'],
       //componentRestrictions: {country: 'au'}
-  };
+    };
 
-  // Create the autocomplete object
-  var autocomplete = new google.maps.places.Autocomplete(input, options);
+// Create the autocomplete object
+var autocomplete = new google.maps.places.Autocomplete(input, options);
 
-  autocomplete.bindTo('bounds', map);
+autocomplete.bindTo('bounds', map);
 
-  autocomplete.setFields(
-    ['address_components', 'geometry', 'icon', 'name']);
+autocomplete.setFields(
+  ['address_components', 'geometry', 'icon', 'name']);
 
-  var infowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
-  infowindow.setContent(infowindowContent);
-  var marker = new google.maps.Marker({
-    map:map,
-    anchorPoint: new google.maps.Point(0, -29)
-  });
+var infowindow = new google.maps.InfoWindow();
+var infowindowContent = document.getElementById('infowindow-content');
+infowindow.setContent(infowindowContent);
+var marker = new google.maps.Marker({
+  map:map,
+  //icon: carMarker,
+  anchorPoint: new google.maps.Point(0, -29)
+});
 
-       autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
-            return;
-          }
+autocomplete.addListener('place_changed', function() {
+  infowindow.close();
+  marker.setVisible(false);
+  var place = autocomplete.getPlace();
+  // if the location not found, it will prompt a dialog box to let user know
+  if (!place.geometry) {
+    window.alert("The location you search are not found: '" + place.name + "'");
+    return;
+  }
 
-          // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-          }
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
-
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
-
-          infowindowContent.children['place-icon'].src = place.icon;
-          infowindowContent.children['place-name'].textContent = place.name;
-          infowindowContent.children['place-address'].textContent = address;
-          infowindow.open(map, marker);
-        });
-
-        // Sets a listener on a radio button to change the filter type on Places
-        // Autocomplete.
-        function setupClickListener(id, types) {
-          var radioButton = document.getElementById(id);
-          radioButton.addEventListener('click', function() {
-            autocomplete.setTypes(types);
-          });
-        }
-
-        setupClickListener('changetype-all', []);
-        setupClickListener('changetype-address', ['address']);
-        setupClickListener('changetype-establishment', ['establishment']);
-        setupClickListener('changetype-geocode', ['geocode']);
-
-        document.getElementById('use-strict-bounds')
-            .addEventListener('click', function() {
-              console.log('Checkbox clicked! New state=' + this.checked);
-              autocomplete.setOptions({strictBounds: this.checked});
-            });
-
+// If location found then show on map
+if (place.geometry.viewport) {
+  map.fitBounds(place.geometry.viewport);
+} else {
+  map.setCenter(place.geometry.location);
+  map.setZoom(17);
 }
 
- 
+// It will set the location you search with red marker
+marker.setPosition(place.geometry.location);
+marker.setVisible(true);
 
+var address = '';
+if (place.address_components) {
+  address = [
+  (place.address_components[0] && place.address_components[0].short_name || ''),
+  (place.address_components[1] && place.address_components[1].short_name || ''),
+  (place.address_components[2] && place.address_components[2].short_name || '')
+  ].join(' ');
+}
+
+infowindowContent.children['place-icon'].src = place.icon;
+infowindowContent.children['place-name'].textContent = place.name;
+infowindowContent.children['place-address'].textContent = address;
+infowindow.open(map, marker);
+
+});
+
+}
