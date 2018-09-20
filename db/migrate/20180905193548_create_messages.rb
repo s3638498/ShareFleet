f980dabbd07c70ad418167d81df063cbc664ede3
Ryan Tran <s3201690@student.rmit.edu.au>
2018-09-20 06:14:27 +0000

    Removed Enduser and admin from migrate

--- db/migrate/20180905193548_create_messages.rb
+++ db/migrate/20180905193548_create_messages.rb
@@ -3,12 +3,7 @@ class CreateMessages < ActiveRecord::Migration[5.2]
     create_table :messages do |t|
       t.text :content
       t.string :author
-      t.references :administrator, foreign_key: false
-      t.references :enduser, foreign_key: false
-
       t.timestamps
     end
-    add_foreign_key :messages, :administrator, column: :enduser_id
-    add_foreign_key :messages, :enduser, column: :administrator_id
   end
 end
