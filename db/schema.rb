4fbfe36135d0636e9b33041efbd3926ca0892777
Ryan Tran <s3201690@student.rmit.edu.au>
2018-09-23 12:02:22 +0000

    Add rate to Vehicle

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_055800) do
+ActiveRecord::Schema.define(version: 2018_09_23_120137) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2018_09_19_055800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "location_id"
+    t.float "rate"
     t.index ["location_id"], name: "index_vehicles_on_location_id"
   end
 
