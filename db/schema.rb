df6cf9d616995301c18f6ae4543f5f07c0b1b22b
Ryan Tran <s3201690@student.rmit.edu.au>
2018-09-23 12:06:45 +0000

    Added Total cost of booking to Bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_23_120137) do
+ActiveRecord::Schema.define(version: 2018_09_23_120436) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_09_23_120137) do
     t.datetime "updated_at", null: false
     t.integer "vehicle_id"
     t.integer "user_id"
+    t.float "total"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["vehicle_id"], name: "index_bookings_on_vehicle_id"
   end
