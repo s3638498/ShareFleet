b1f4a50d515c94faff64089de70cb6ffce476e8b
Ryan Tran <s3201690@student.rmit.edu.au>
2018-09-27 18:03:40 +0000

    Created Promotion model and migration

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,8 +4,12 @@ class InvitationsController < ApplicationController
     end
     
     def create
-        UserMailer.sign_up(invite_params).deliver
-        flash.now[:success] = "Invite sent!"
+        if !invite_params[:recipient_email].blank?
+            UserMailer.sign_up(invite_params).deliver
+            flash.now[:success] = "Invite sent!"
+        else
+            flash.now[:warning] = "Cannot send a empty email!"
+        end
         render 'new'
     end
 
