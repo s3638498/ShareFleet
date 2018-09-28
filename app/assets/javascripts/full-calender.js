7df9a1607780532bbf950440eca966fc23c08149
Ryan Tran <s3201690@student.rmit.edu.au>
2018-09-28 18:57:37 +0000

    Working dynamic total

--- app/assets/javascripts/full-calender.js
+++ app/assets/javascripts/full-calender.js
@@ -14,7 +14,7 @@ $(document).on('turbolinks:load', function(){
 
     $('#calendar').fullCalendar({
         header: {
-            left: 'prev,next today',
+            left: 'next today',
             center: 'title',
             right: 'month,agendaDay'
         },
@@ -22,11 +22,21 @@ $(document).on('turbolinks:load', function(){
         selectOverlap: false,
         defaultView: 'month',
         events: events_array,
-        
+        //Hide all events in month view
+        eventRender: function(event, element, view) {
+		if(view.type == 'month') {
+			$(element).css("display", "none");
+		}},
+		//On Click event for month go to the selected day agenda.
         dayClick: function(date, jsEvent, view) {
-            $('#calendar').fullCalendar('gotoDate',date);
-            $('#calendar').fullCalendar('changeView','agendaDay');
+            var today = new Date();
+            today.setDate(today.getDate());
+            if ( date >= today ) {
+                $('#calendar').fullCalendar('gotoDate',date);
+                $('#calendar').fullCalendar('changeView','agendaDay');
+            }
         },
+        //Make selectable events.
         select: function(start, end, jsEvent, view) {
             console.log(view.name);
 			if ( view.name === "agendaDay") {
@@ -37,8 +47,11 @@ $(document).on('turbolinks:load', function(){
                 alert(["Event Start date: " + moment(start).format(),
                 "Event End date: " + moment(end).format(),
                 "AllDay: " + allDay].join("\n"));
+                document.getElementById('pickup_datetime').value = moment(start).toISOString();
+                document.getElementById('drop_datetime').value = moment(end).toISOString();
+                var totalTime = (moment(end).unix() - moment(start).unix())/3600;
+                document.getElementById('booking_total').value = (totalTime * vec_rate).toFixed(2);;
 			}
         }
-        
     });
 });
